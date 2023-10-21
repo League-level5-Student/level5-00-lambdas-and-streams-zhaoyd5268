eae9b75039529935fd9f402e9fd79a43a12bf192
zhaoyd5268 <zhaoyd5268@gmail.com>
2023-10-21 12:55:35 -0700

    Finish LambdaMethods

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,6 +1,7 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,14 +21,28 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((e) -> {
+			System.out.println("Since you pressed this button, 175,983,958,767+2,938,575,738 = 178,922,534,505");
+		});
+		randNumber.addActionListener((e) -> {
+			Random ran = new Random();
+			int r = ran.nextInt(1000);
+			System.out.println("Since you pressed this button, a random nmber has been generated from 0-1000. It is: " + r+".");
+		});
+		tellAJoke.addActionListener((e) -> {
+			System.out.println("Why did the chicken cross the road? \n \n \n \n \n \n \n \n \n \n \n "
+					+ "To get to the other side!");
+
+		});
 		window.setVisible(true);
 		window.pack();
+		
 	}
 	
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
 	
 	
 }
